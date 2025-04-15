5b8a350bd471d88a99d56dea3ad87e4e862fc489
timmeyer1 <t>
2025-04-15 13:46:51 +0200

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Stagiaire on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
